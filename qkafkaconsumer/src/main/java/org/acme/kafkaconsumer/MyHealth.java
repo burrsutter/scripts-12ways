622c82bf6454c49ba9b1eefac8787198b2802b71
Burr Sutter <burrsutter@gmail.com>
2019-03-23 13:10:34 -0400

    moving properties, adding health

---
+++ qkafkaconsumer/src/main/java/org/acme/kafkaconsumer/MyHealth.java
@@ -0,0 +1,10 @@
+package org.acme.kafkaconsumer;
+
+import org.eclipse.microprofile.health.Health;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@Health
+public class MyHealth {
+
+}
--- qkafkaconsumer/src/main/java/org/acme/kafkaconsumer/MyStreamSubscriber.java
+++ qkafkaconsumer/src/main/java/org/acme/kafkaconsumer/MyStreamSubscriber.java
@@ -3,15 +3,24 @@ package org.acme.kafkaconsumer;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
-import org.eclipse.microprofile.health.Health;
+import java.util.concurrent.CompletionStage;
 
-@Health
+import io.smallrye.reactive.messaging.kafka.KafkaMessage;
+
 @ApplicationScoped
 public class MyStreamSubscriber {
     private static final Logger LOG = Logger.getLogger(MyStreamSubscriber.class);
 
+/* 
     @Incoming("mystream")
     public void process(String msg) {
         LOG.info(msg);
-    }    
+    }     
+*/
+
+    @Incoming("mystream")
+    public CompletionStage<Void> process(KafkaMessage<String,String> msg) {
+        LOG.info(msg.getPayload());
+        return msg.ack();
+    }
 }
