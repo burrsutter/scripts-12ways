2c9e0b568e1b9685d862cb8efee96bd4dee61028
Burr Sutter <burrsutter@gmail.com>
2019-04-14 10:00:30 -0400

    adding Kafka producer/consumer scripts

---
+++ 17_kafka_producer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl run kafka-producer -ti --image=strimzi/kafka:0.11.1-kafka-2.1.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list my-cluster-kafka-bootstrap:9092 --topic my-topic 
